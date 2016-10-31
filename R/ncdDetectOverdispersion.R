59f216e0a9ffdf8d6e865307752adbeffe43a5e0
Malene Juul Rasmussen <malenejuulrasmussen@gmail.com>
2016-10-31 16:02:23 -0400

    Multiple columns in prediction and socre matrices

--- R/ncdDetectOverdispersion.R
+++ R/ncdDetectOverdispersion.R
@@ -21,24 +21,17 @@ logistic <- function(x){
 #' convolution(dat = data.table(x = c(rep(1, 2), rep(2, 3), rep(3, 4)), y = c(1:2, 1:3, 1:4), 
 #'     probability = c(3/4, 1/4, rep(1/3, 3), (1:4)/10)), threshold = 6)
 #' @export
-ncdDetectOverdispersion <- function(predictions, scores, overdispersion = 0, N = 10, method = c("naive", "is", "numeric"), observations = NA, thres = NA) {
+
+
+ncdDetectOverdispersion <- function(predictions, scores, overdispersion = 0, N = 10, method = c("naive", "is", "numeric"), observed_score, return_dist = F) {
+  
+  
   # Make initial checks on input --------------------------------------------
   
-  observation_available <- F
-  if (!is.na(observations[1])) {
-    observation_available <- T
-  }
-  
-  
+
   # - data dimenstions
   if (!identical(nrow(predictions), nrow(scores))) {
-    stop("the row numbers of predictions, scores and observations must be identical")
-  }
-  
-  if (observation_available) {
-    if (!identical(nrow(predictions), nrow(observations))) {
-      stop("the row numbers of predictions, scores and observations must be identical") 
-    }
+    stop("the row numbers of predictions and scores must be identical")
   }
   
   # - negative scores (cannot be handled)
@@ -47,76 +40,66 @@ ncdDetectOverdispersion <- function(predictions, scores, overdispersion = 0, N =
   }
   
   # row sums of predictions matrix
-  if (unique(rowSums(predictions)) != 1) {
+  if (unique(round(rowSums(predictions), 10)) != 1) {
     stop("the row sums of the prediction matrix must sum to one")
   }
   
-  # row sums of observations must sum to one (must contain only one 1; the rest must be zero)
-  if (observation_available) {
-    if (unique(rowSums(observations)) != 1) {
-      stop("each row in the observation matrix must contain one 1, with the rest being zeros")
-    }
-  }
-  
-  
-  
+
   
   # Convert data into data.tables -------------------------------------------
   
   predictions <- as.data.table(predictions)
   scores <- data.table(scores)
-  if (observation_available) {
-    observations <- data.table(observations)
-  }
+
+  
   
   # Add x-values to data ----------------------------------------------------  
   # (needed in the convolution step in order to know which positions go together)
   
-  predictions[, x := 1:.N]
   scores[, x := 1:.N]
-  if (observation_available) {
-    observations[, x := 1:.N]
-  }
+
   
   
-  # Get data in long format -------------------------------------------------
-  
+  # Get scores in long format -------------------------------------------------
 
   scores_long <- melt(scores, id.vars = c("x"), measure.vars = setdiff(names(scores), "x"),
                       variable.name = "mutation_type", value.name = "y", variable.factor = F)
   
-  if (observation_available) {
-    observations_long <- melt(observations, id.vars = c("x"), measure.vars = setdiff(names(observations), "x"),
-                              variable.name = "mutation_type", value.name = "observation", variable.factor = F)
-  }
-  
-  
-  # Combine predictions, scores and observations ----------------------------
-  
+
   # - set key columns for merging
   setkeyv(scores_long, c("x", "mutation_type"))
   
   
+
+  # Integrate out random effect ---------------------------------------------
+
+  
+  conv_results <- vector("list", N)
   
   
-  ###
-  # Integrate out random effect
-  ###
-  conv_results <- list()
-  for(i in 1:N){
+  for (i in 1:N) {
+    
+    # - print progress
+    cat(paste("calculating p-values using overdispersion. Iteration ", i, " of ", N, ".\n", sep = ""))
+    
+    # - randomly draw random effect
     random_effect <- rnorm(1, 0, sd = overdispersion)
     
-    pred_cols <- setdiff(names(predictions), "x")[-1]
-    pred_cols <- setdiff(names(predictions), "x")
-    
+    # - add random effect to predictions in logit space
+    pred_cols <- names(predictions)[-1]
     pred_od <- data.table(logistic(logit(as.matrix(predictions[, pred_cols, with = F])) + random_effect))
+
+    # - add column with probability of no mutation    
+    no_mut_prob <- data.table(NO = 1-rowSums(pred_od))
+    setnames(no_mut_prob, names(predictions)[1])
+             
+    pred_od <- cbind(no_mut_prob, pred_od)
     
-    ### malene fikser
-    pred_od[, V1 := 1 - (V2 + V3)]
     
+    # - add x-values to predictions
     pred_od[, x := 1:.N]
     
-    
+    # - get predictions in long format
     predictions_long <- melt(pred_od, id.vars = c("x"), measure.vars = setdiff(names(pred_od), "x"),
                              variable.name = "mutation_type", value.name = "probability", variable.factor = F)
     
@@ -128,40 +111,98 @@ ncdDetectOverdispersion <- function(predictions, scores, overdispersion = 0, N =
     dat <- predictions_long[scores_long]
     setkeyv(dat, c("x", "mutation_type"))
     
-    # - finally, merge observations onto the data
-    if (observation_available) {
-      setkeyv(observations_long, c("x", "mutation_type"))
-      dat <- dat[observations_long]
-    }
-    
+
    
-    # Perform convolution ----------------------------------------------------- 
-    conv_results[[i]] <- convolution(dat, threshold = thres)
+    # -perform convolution
+    conv_results[[i]] <- convolution(dat, threshold = max(observed_score))
+    
+    
+    # - clean up
+    rm(random_effect, pred_cols, pred_od, no_mut_prob, predictions_long, dat)
     
   }
   
-  # Average over results
-  conv_results_df <- suppressWarnings(Reduce(function(x,y){merge(as.data.frame(x),as.data.frame(y), all = T, by = "y")}, conv_results))
-  conv_results_df[is.na(conv_results_df)] <- 0
-  score_dist <- rowMeans(conv_results_df[,-1])
+  # average over results
+  score_dist <- rbindlist(conv_results)[, .("probability" = sum(probability)/N), by = y]
   
-  # Get observed score and p-value ------------------------------------------
+   
+# Alternative
+#   conv_results_df <- suppressWarnings(Reduce(function(x,y){merge(as.data.frame(x),as.data.frame(y), all = T, by = "y")}, conv_results))
+#   conv_results_df[is.na(conv_results_df)] <- 0
+#   score_dist <- rowMeans(conv_results_df[,-1])
+   
   
-  if (observation_available) {
+  
+  # Calculate score distribution without overdispersion ---------------------
+  
+  # - add x-values to predictions
+  predictions[, x := 1:.N]
+  
+  # - get predictions in long format
+  predictions_long <- melt(predictions, id.vars = c("x"), measure.vars = setdiff(names(predictions), "x"),
+                           variable.name = "mutation_type", value.name = "probability", variable.factor = F)
+  
+  
+  # - set key columns for merging
+  setkeyv(predictions_long, c("x", "mutation_type"))
+  
+  # - merge predictions and scores; set key columns
+  dat <- predictions_long[scores_long]
+  setkeyv(dat, c("x", "mutation_type"))
+  
+
+  # - perform convolution 
+  score_dist_no_overdispersion <- convolution(dat, threshold = max(observed_score))
+  
+  
+
+
+  # Get p-value(s) ----------------------------------------------------------
+  
+  # - with overdispersion
+  output_od <- vector("list", length(observed_score))
+  output_index <- 0
+  
+  for (obs_score in observed_score) {
     
-    obs_score <- dat[observation == 1, sum(y)]
+    output_index <- output_index + 1
     
-    p_value <- mean(sapply(conv_results, FUN = function(score_dist_df){score_dist_df[y >= 10, sum(probability)]}))
+    output_od[[output_index]] <- data.table(score = obs_score, p_value = score_dist[y < obs_score, 1 - sum(probability)], type = method)
     
   }
+  
+  output_od <- rbindlist(output_od)
+  
+  # - without overdispersion
+  output <- vector("list", length(observed_score))
+  output_index <- 0
+  
+  for (obs_score in observed_score) {
+    
+    output_index <- output_index + 1
+    
+    output[[output_index]] <- data.table(score = obs_score, p_value = score_dist_no_overdispersion[y < obs_score, 1 - sum(probability)], 
+                                         type = "no_overdispersion")
+    
+  }
+  
+  output <- rbindlist(output)
   
   
   
   # Return results ----------------------------------------------------------
   
-  if (observation_available) {
-    return(list("score_dist" = score_dist, "obs_score" = obs_score, "p_value" = p_value))  
+  if (return_dist) {
+    
+    return(list(dist = score_dist, output = rbind(output_od, output)))
+           
   } else {
-    return(list("score_dist" = score_dist))  
+      
+      return(rbind(output_od, output))
+    
   }
+    
 }
+
+
+
